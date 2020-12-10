fae78ba3d6e068f3d4c6f3f04a66edb2f948be1c
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2020-12-10 08:10:41 +0100

    Legg til flere muligheter i velkomstmelding

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,21 @@ import Panel from "nav-frontend-paneler";
 import { Normaltekst, Systemtittel } from "nav-frontend-typografi";
 
 function App() {
-  function greeting() {
-    return new Date().getHours() > 12 ? "God ettermiddag" : "God formiddag";
+  function getGreeting() {
+    const hour = new Date().getHours();
+
+    if (hour < 5) return "God natt";
+    if (hour < 10) return "God morgen";
+    if (hour < 12) return "God formiddag";
+    if (hour < 18) return "God ettermiddag";
+    if (hour < 22) return "God kveld";
+    return "God natt";
   }
 
   return (
     <div className="podlet-veientilarbeid">
       <Panel border>
-        <Systemtittel>{greeting()}</Systemtittel>
+        <Systemtittel>{getGreeting()}</Systemtittel>
         <Normaltekst>Du har nå kommet deg inn i NAVs nye microfrontend.</Normaltekst>
       </Panel>
     </div>
