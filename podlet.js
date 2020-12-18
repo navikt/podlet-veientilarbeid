605d011910b6614d68de2b3fdedb5852767c8669
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2020-12-18 08:08:03 +0100

    Legger til prometheus-metrics i podlet

--- podlet.js
+++ podlet.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const Podlet = require("@podium/podlet");
 const fs = require("fs");
+const promClient = require("prom-client");
+const PrometheusConsumer = require("@metrics/prometheus-consumer");
 
 const basePath = process.env.BASE_PATH || "/arbeid/podlet-veientilarbeid";
 const port = process.env.PORT || 7100;
@@ -11,8 +13,6 @@ const podletName = "podlet-veientilarbeid";
 
 let rawdata = fs.readFileSync("build/asset-manifest.json");
 let assets = JSON.parse(rawdata);
-
-const app = express();
 
 const podlet = new Podlet({
   name: podletName,
@@ -30,6 +30,15 @@ assets.entrypoints.forEach((element, index) => {
     podlet.js({ value: `/${element}`, defer: true });
   }
 });
+
+// Set up prometheus client with podium metrics
+promClient.collectDefaultMetrics();
+const metricsConsumer = new PrometheusConsumer({ client: promClient });
+metricsConsumer.on("error", (err) => console.error(err));
+podlet.metrics.pipe(metricsConsumer);
+
+// Express setup
+const app = express();
 
 app.use(podlet.middleware());
 app.use("/static", express.static("./build/static"));
@@ -55,6 +64,12 @@ app.get(`${basePath}${podlet.fallback()}`, (req, res) => {
 // generate the podlet manifest
 app.get(`${basePath}${podlet.manifest()}`, (req, res) => {
   res.status(200).send(podlet);
+});
+
+// Prometheus metrics
+app.get("/metrics", async function (req, res) {
+  const metrics = await metricsConsumer.metrics();
+  res.set("Content-Type", metricsConsumer.contentType()).send(metrics);
 });
 
 // isAlive/isReady route for Nais
